001ddb2de31de1ae2d8571c7b878440d4903f500
Adel Knode <ann.knode@atlasschool.com>
2025-03-02 18:21:24 +0000

    data fetching typed but not corrected

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,9 +1,36 @@
-export default async function Page({ params }: { params: { id: string } }) {
+import { AskQuestion } from "@/components/AskQuestion";
+import { Question } from "@/components/Question";
+import { fetchQuestions, fetchTopic } from "@/lib/data";
+import { HashtagIcon } from "@heroicons/react/24/outline";
+
+export default async function Page({ 
+    params,
+ }: { 
+        params: Promise<{ id: string }>;
+     }) {
     const { id } = params;
+    const topic = await fetchTopic(id);
+    const questions = await fetchQuestions(id);
+
+    if (!topic) {
+        return <div> Top Pic Not Found </div>;
+    }
     
     return (
-        <div> 
-            Top Picks Page: {id} 
+        <div>
+            <h1 className="text-3x1 font-black flex items-center">
+                <HastagIcon className="h-6 w-6 mr-2" /> {topic.title}
+                </h1>
+                <AskQuestion topic={topic.id} />
+            {questions.map((question)
+        =>(
+            <Question
+            key={question.id}
+            id={question.id}
+            text={question.title}
+            votes={question.votes}
+            />
+        ))} 
         </div>
     );
 }
