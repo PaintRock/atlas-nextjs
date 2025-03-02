10e7c19825fa9ba135a1c61c80d85b7c50b41890
Adel Knode <ann.knode@atlasschool.com>
2025-03-02 17:53:31 +0000

    removed await from id-page

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,14 +1,9 @@
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-    // Await the params before using them
-    const resolvedParams = await params;
-    const { id } = resolvedParams;
-  
-    await new Promise((r) => setTimeout(r, 3000));
+export default async function Page({ params }: { params: { id: string } }) {
+    const { id } = params;
     
     return (
-      <div> 
-        Top Picks Page: {resolvedParams.id} 
-      </div>
+        <div> 
+            Top Picks Page: {id} 
+        </div>
     );
-  }
-  
+}
